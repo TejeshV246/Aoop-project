5ba4576c0369cd2728098e5d1b58becd5da450cc
TejeshV246 <2320030246@klh.edu.in>
2024-08-06 11:17:40 +0530

    Add files via upload

---
+++ project.java/Coffee.java
@@ -0,0 +1,17 @@
+public class Coffee {
+    private String name;
+    private double BasePrice;
+
+    public Coffee(String name, double BasePrice) {
+        this.name = name;
+        this.BasePrice = BasePrice;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getBasePrice() {
+        return BasePrice;
+    }
+}
---
+++ project.java/Condiment.java
@@ -0,0 +1,17 @@
+public class Condiment {
+    private String name;
+    private double price;
+
+    public Condiment(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
